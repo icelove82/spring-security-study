cb1467a4bc0f7e116e34f6b12a15b399a000d371
YUN <yunmyeonghun@gmail.com>
2021-08-25 14:38:12 +0900

    07 Init insert data and query api

--- src/main/java/com/ymh/springsecuritystudy/SpringSecurityStudyApplication.java
+++ src/main/java/com/ymh/springsecuritystudy/SpringSecurityStudyApplication.java
@@ -1,13 +1,56 @@
 package com.ymh.springsecuritystudy;
 
+import com.ymh.springsecuritystudy.domain.Role;
+import com.ymh.springsecuritystudy.domain.User;
+import com.ymh.springsecuritystudy.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityStudyApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserService userService) {
+        return args -> {
+            userService.saveRole(Role.builder().name("ROLE_USER").build());
+            userService.saveRole(Role.builder().name("ROLE_MANGER").build());
+            userService.saveRole(Role.builder().name("ROLE_ADMIN").build());
+            userService.saveRole(Role.builder().name("ROLE_SUPER_ADMIN").build());
+
+            userService.saveUser(User.builder()
+                    .name("John Travolta")
+                    .username("john")
+                    .password("1234").build());
+
+            userService.saveUser(User.builder()
+                    .name("Will Smith")
+                    .username("will")
+                    .password("1234").build());
+
+            userService.saveUser(User.builder()
+                    .name("Jim Carry")
+                    .username("jim")
+                    .password("1234").build());
+
+            userService.saveUser(User.builder()
+                    .name("Arnold Schwarzenegger")
+                    .username("arnold")
+                    .password("1234").build());
+
+            userService.addRoleToUser("john", "ROLE_USER");
+            userService.addRoleToUser("john", "ROLE_MANGER");
+            userService.addRoleToUser("will", "ROLE_MANGER");
+            userService.addRoleToUser("jim", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_USER");
+        };
     }
 
 }
--- src/main/java/com/ymh/springsecuritystudy/domain/Role.java
+++ src/main/java/com/ymh/springsecuritystudy/domain/Role.java
@@ -1,6 +1,7 @@
 package com.ymh.springsecuritystudy.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import static javax.persistence.GenerationType.AUTO;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role {
